b3ffdea2bdb991a9013421f920f47f33f04016b3
Zhou Yuchao <513629287@qq.com>
2013-11-30 21:23:09 -0600

    add judgement

--- src/Frame.java
+++ src/Frame.java
@@ -3,8 +3,6 @@
 import matlabTool.*;
 import com.mathworks.toolbox.javabuilder.*;
 
-
-
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -25,16 +23,15 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.util.regex.Pattern;
 
-class Frame extends JFrame implements ActionListener
-{
+class Frame extends JFrame implements ActionListener {
 	public static final int DEFAULT_WIDTH = 490;
 	public static final int DEFAULT_HEIGHT = 490;
-	
+
 	JLabel intro = new JLabel();
 	JLabel introPic = new JLabel();
 	JLabel queryImage = new JLabel();
 	JLabel[] Image = new JLabel[24];
-	
+
 	JButton offset;
 	JButton previous;
 	JButton next;
@@ -43,45 +40,44 @@ class Frame extends JFrame implements ActionListener
 	JPanel panel3;
 	JPanel introduction;
 	JPanel bottom;
-	
+
 	String picName = "";
 	String histogram = "";
 	String distance = "";
 	String databasePath = "";
-	
-	
-	/*connect the matlab*/
+
+	/* connect the matlab */
 	Tool tool = null;//
-	private Object[] result = null;//	
-	
-	public Frame() 
-	{
-		String[] distanceList = {"", "Euclidean distance", "Intersection distance"};
-		String[] histogramList = {"", "Global color histogram", "Segment color histogram"};
-		
+	private Object[] result = null;//
+
+	public Frame() {
+		String[] distanceList = { "", "Euclidean distance",
+				"Intersection distance" };
+		String[] histogramList = { "", "Global color histogram",
+				"Segment color histogram" };
+
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		setTitle("ImageGoogle");
 		setResizable(true);
 
-		
 		Toolkit toolkit = this.getToolkit();
-		//Image img=toolkit.createImage( this.getClass().getResource("/image/icon.png"));
-		//this.setIconImage(img);//set the demo image
-		
-		//图片
-		queryImage.setPreferredSize(new Dimension(160,160));
+		// Image img=toolkit.createImage(
+		// this.getClass().getResource("/image/icon.png"));
+		// this.setIconImage(img);//set the demo image
+
+		// ��剧��
+		queryImage.setPreferredSize(new Dimension(160, 160));
 		queryImage.setBorder(BorderFactory.createLineBorder(Color.gray));
-		for (int i = 0; i < Image.length; i++)
-		{
+		for (int i = 0; i < Image.length; i++) {
 			Image[i] = new JLabel();
-			Image[i].setPreferredSize(new Dimension(160,160));
+			Image[i].setPreferredSize(new Dimension(160, 160));
 			Image[i].setBorder(BorderFactory.createLineBorder(Color.gray));
-//			Image[i].setText("Image"+i);
+			// Image[i].setText("Image"+i);
 		}
-		
+
 		intro = new JLabel("welcome");
-		
-		//buttons
+
+		// buttons
 		JButton uploadButton = new JButton("Upload");
 		JButton databaseButton = new JButton("Database");
 		JLabel query = new JLabel("Query Image: ");
@@ -90,42 +86,38 @@ class Frame extends JFrame implements ActionListener
 		databaseButton.addActionListener(this);
 		uploadButton.addActionListener(this);
 		search.addActionListener(this);
-		
+
 		offset = new JButton("offset");
 		previous = new JButton("Previous");
 		next = new JButton("Next");
 		previous.addActionListener(this);
 		next.addActionListener(this);
-		
-		//ComnoBox
+
+		// ComnoBox
 		JComboBox histogramBox = new JComboBox(histogramList);
-		histogramBox.addItemListener(new ItemListener() 
-		{
-            @Override
-            public void itemStateChanged(ItemEvent e) 
-            {
-                if(e.getStateChange() == 1) {
-                	histogram = e.getItem().toString();
-                	System.out.println(e.getItem().toString());
-                }
-            }
-         
-        });
-		
+		histogramBox.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == 1) {
+					histogram = e.getItem().toString();
+					System.out.println(e.getItem().toString());
+				}
+			}
+
+		});
+
 		JComboBox distanceBox = new JComboBox(distanceList);
-		distanceBox.addItemListener(new ItemListener() 
-		{
-            @Override
-            public void itemStateChanged(ItemEvent ie) 
-            {
-                if(ie.getStateChange() == 1) {
-                	distance = ie.getItem().toString();
-                	System.out.println(ie.getItem().toString());
-                }
-            }
-        });
-		
-		//Panels
+		distanceBox.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent ie) {
+				if (ie.getStateChange() == 1) {
+					distance = ie.getItem().toString();
+					System.out.println(ie.getItem().toString());
+				}
+			}
+		});
+
+		// Panels
 		basicPanel = new JPanel();
 		JPanel panel1 = new JPanel();
 		JPanel panel1_uper = new JPanel();
@@ -133,92 +125,89 @@ class Frame extends JFrame implements ActionListener
 		panel3 = new JPanel();
 		introduction = new JPanel();
 		bottom = new JPanel();
-		
+
 		basicPanel.setLayout(new BorderLayout());
 		panel1_uper.setLayout(new GridBagLayout());
 		setContentPane(basicPanel);
 		panel1.setLayout(new FlowLayout());
 		basicPanel.add(introduction, "Center");
 		basicPanel.add(panel1, "North");
-		//basicPanel.add(panel2, "Center");
+		// basicPanel.add(panel2, "Center");
 		basicPanel.add(bottom, "South");
 		panel1.add(queryImage);
 		panel1.add(panel1_uper);
 		GridBagConstraints co = new GridBagConstraints();
-		addComponent(panel1_uper, query, co, 0, 0, 1 ,1);
-		addComponent(panel1_uper, uploadButton, co, 1, 0, 1 ,1);
-		addComponent(panel1_uper, databse, co, 0, 1, 1 ,1);
-		addComponent(panel1_uper, databaseButton, co, 1, 1, 1 ,1);
-		//panel1.add(databaseButton);
-		//panel1.add(uploadButton);
+		addComponent(panel1_uper, query, co, 0, 0, 1, 1);
+		addComponent(panel1_uper, uploadButton, co, 1, 0, 1, 1);
+		addComponent(panel1_uper, databse, co, 0, 1, 1, 1);
+		addComponent(panel1_uper, databaseButton, co, 1, 1, 1, 1);
+		// panel1.add(databaseButton);
+		// panel1.add(uploadButton);
 		panel1.add(histogramBox);
 		panel1.add(distanceBox);
 		panel1.add(search);
 		introduction.setLayout(new BorderLayout());
-		introduction.setPreferredSize(new Dimension(532, 765));//锟截硷拷锟斤拷锟�,锟斤拷锟斤拷JPanel锟侥达拷小  
+		introduction.setPreferredSize(new Dimension(532, 765));// ������纭锋�烽����ゆ�烽��锟�,�����ゆ�烽����ゆ��JPanel���渚ヨ揪��峰��
 		introduction.add(intro, "East");
 		introduction.add(introPic, "West");
-		
-		
-		//使锟斤拷GridLayout时锟睫凤拷锟教讹拷JLabel锟侥达拷小锟斤拷锟斤拷小锟斤拷锟斤拷锟脚达拷锟节的改憋拷锟侥憋拷,
-		//锟斤拷锟斤拷只锟斤拷使锟斤拷GridBagLayout
+
+		// 浣块����ゆ��GridLayout��堕�������ゆ�烽�����璁规��JLabel���渚ヨ揪��峰�������ゆ�烽����ゆ�峰�������ゆ�烽����ゆ�烽�����杈炬�烽����������规����烽��渚ユ�����,
+		// �����ゆ�烽����ゆ�峰�������ゆ�蜂娇�����ゆ��GridBagLayout
 		panel2.setLayout(new GridBagLayout());
 		GridBagConstraints con = new GridBagConstraints();
-		panel2.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		panel2.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		panel2.setBorder(BorderFactory.createTitledBorder("Result"));
-		addComponent(panel2, Image[0], con, 0, 0, 1 ,1);
-		addComponent(panel2, Image[1], con, 1, 0, 1 ,1);
-		addComponent(panel2, Image[2], con, 2, 0, 1 ,1);
-		addComponent(panel2, Image[3], con, 3, 0, 1 ,1);
-		addComponent(panel2, Image[4], con, 0, 1, 1 ,1);
-		addComponent(panel2, Image[5], con, 1, 1, 1 ,1);
-		addComponent(panel2, Image[6], con, 2, 1, 1 ,1);
-		addComponent(panel2, Image[7], con, 3, 1, 1 ,1);
-		addComponent(panel2, Image[8], con, 0, 2, 1 ,1);
-		addComponent(panel2, Image[9], con, 1, 2, 1 ,1);
-		addComponent(panel2, Image[10], con, 2, 2, 1 ,1);
-		addComponent(panel2, Image[11], con, 3, 2, 1 ,1);
-		
-		
+		addComponent(panel2, Image[0], con, 0, 0, 1, 1);
+		addComponent(panel2, Image[1], con, 1, 0, 1, 1);
+		addComponent(panel2, Image[2], con, 2, 0, 1, 1);
+		addComponent(panel2, Image[3], con, 3, 0, 1, 1);
+		addComponent(panel2, Image[4], con, 0, 1, 1, 1);
+		addComponent(panel2, Image[5], con, 1, 1, 1, 1);
+		addComponent(panel2, Image[6], con, 2, 1, 1, 1);
+		addComponent(panel2, Image[7], con, 3, 1, 1, 1);
+		addComponent(panel2, Image[8], con, 0, 2, 1, 1);
+		addComponent(panel2, Image[9], con, 1, 2, 1, 1);
+		addComponent(panel2, Image[10], con, 2, 2, 1, 1);
+		addComponent(panel2, Image[11], con, 3, 2, 1, 1);
+
 		panel3.setLayout(new GridBagLayout());
-		panel3.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-		panel3.setBorder(BorderFactory.createTitledBorder("Result"));		
-		addComponent(panel3, Image[12], con, 0, 0, 1 ,1);
-		addComponent(panel3, Image[13], con, 1, 0, 1 ,1);
-		addComponent(panel3, Image[14], con, 2, 0, 1 ,1);
-		addComponent(panel3, Image[15], con, 3, 0, 1 ,1);
-		addComponent(panel3, Image[16], con, 0, 1, 1 ,1);
-		addComponent(panel3, Image[17], con, 1, 1, 1 ,1);
-		addComponent(panel3, Image[18], con, 2, 1, 1 ,1);
-		addComponent(panel3, Image[19], con, 3, 1, 1 ,1);
-		addComponent(panel3, Image[20], con, 0, 2, 1 ,1);
-		addComponent(panel3, Image[21], con, 1, 2, 1 ,1);
-		addComponent(panel3, Image[22], con, 2, 2, 1 ,1);
-		addComponent(panel3, Image[23], con, 3, 2, 1 ,1);
-		
-		
+		panel3.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		panel3.setBorder(BorderFactory.createTitledBorder("Result"));
+		addComponent(panel3, Image[12], con, 0, 0, 1, 1);
+		addComponent(panel3, Image[13], con, 1, 0, 1, 1);
+		addComponent(panel3, Image[14], con, 2, 0, 1, 1);
+		addComponent(panel3, Image[15], con, 3, 0, 1, 1);
+		addComponent(panel3, Image[16], con, 0, 1, 1, 1);
+		addComponent(panel3, Image[17], con, 1, 1, 1, 1);
+		addComponent(panel3, Image[18], con, 2, 1, 1, 1);
+		addComponent(panel3, Image[19], con, 3, 1, 1, 1);
+		addComponent(panel3, Image[20], con, 0, 2, 1, 1);
+		addComponent(panel3, Image[21], con, 1, 2, 1, 1);
+		addComponent(panel3, Image[22], con, 2, 2, 1, 1);
+		addComponent(panel3, Image[23], con, 3, 2, 1, 1);
+
 		bottom.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.NONE;
 		c.anchor = GridBagConstraints.CENTER;
 		c.weightx = 4;
 		c.weighty = 4;
-		
+
 		addComponent(bottom, offset, c, 0, 0, 1, 1);
 		offset.setVisible(false);
 		addComponent(bottom, previous, c, 1, 0, 1, 1);
 		addComponent(bottom, next, c, 2, 0, 1, 1);
-		
+
 		previous.setEnabled(false);
 		pack();
 	}
-	
-	//x指锟截硷拷位锟节第硷拷锟斤拷
-	//y指锟截硷拷位锟节第硷拷锟斤拷
-	//w指锟截硷拷锟斤拷要占锟斤拷锟斤拷
-	//h指锟截硷拷锟斤拷要占锟斤拷锟斤拷
-	public void addComponent(JPanel panel, Component c, GridBagConstraints constraints, int x, int y, int w, int h)
-	{
+
+	// x���������纭锋�蜂��������绗�纭锋�烽����ゆ��
+	// y���������纭锋�蜂��������绗�纭锋�烽����ゆ��
+	// w���������纭锋�烽����ゆ�疯����������ゆ�烽����ゆ��
+	// h���������纭锋�烽����ゆ�疯����������ゆ�烽����ゆ��
+	public void addComponent(JPanel panel, Component c,
+			GridBagConstraints constraints, int x, int y, int w, int h) {
 		constraints.insets.bottom = 4;
 		constraints.insets.left = 4;
 		constraints.insets.right = 4;
@@ -229,91 +218,96 @@ class Frame extends JFrame implements ActionListener
 		constraints.gridheight = h;
 		panel.add(c, constraints);
 	}
-	
-	public void actionPerformed(ActionEvent e)
-	{
-		if (e.getActionCommand().equals("Upload"))
-	    {
-			JFileChooser fileChooser = new JFileChooser("G:/EclipseWworkspace/GoogleImage/src/image");
-			FileNameExtensionFilter filter =new FileNameExtensionFilter("Image Files", "jpg","jpeg","gif", "bmp", "png");  
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Upload")) {
+			JFileChooser fileChooser = new JFileChooser(
+					"G:/EclipseWworkspace/GoogleImage/src/image");
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					"Image Files", "jpg", "jpeg", "gif", "bmp", "png");
 			fileChooser.setFileFilter(filter);
-			int result=fileChooser.showOpenDialog(this);
-			if(result==JFileChooser.APPROVE_OPTION)
-			{  
-				String picPath = fileChooser.getSelectedFile().getPath();  
+			int result = fileChooser.showOpenDialog(this);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				String picPath = fileChooser.getSelectedFile().getPath();
 				Pattern pattern = Pattern.compile("/");
 				String[] strs = pattern.split(picPath);
 				picName = strs[strs.length - 1];
 				picName = picPath;
 				System.out.println(picName);
-				ImageIcon icon = new ImageIcon(picPath);  
-				//锟斤拷锟饺憋拷锟斤拷锟斤拷  
-				Image temp = icon.getImage().getScaledInstance(160, 160, icon.getImage().SCALE_DEFAULT);  
+				ImageIcon icon = new ImageIcon(picPath);
+				// �����ゆ�烽��楗烘����烽����ゆ�烽����ゆ��
+				Image temp = icon.getImage().getScaledInstance(160, 160,
+						icon.getImage().SCALE_DEFAULT);
 				icon = new ImageIcon(temp);
-				queryImage.setIcon(icon);  
+				queryImage.setIcon(icon);
 			}
-	    }
-		
-		if (e.getActionCommand().equals("Database"))
-		{
-			JFileChooser directoryChooser = new JFileChooser("c:/");
-			directoryChooser.setFileSelectionMode(1);//设定只能选择到文件夹   
-            int result = directoryChooser.showOpenDialog(this);//此句是打开文件选择器界面的触发语句  
-            if(result==JFileChooser.APPROVE_OPTION)
-			{  
-                File f = directoryChooser.getSelectedFile();//f为选择到的文件  
-                databasePath = f.getAbsolutePath();
-                //System.out.println(f.getAbsolutePath());
-            }
 		}
-	
-		if (e.getActionCommand().equals("Search"))
-		{
-			if (picName == "")
-			{
-				javax.swing.JOptionPane.showMessageDialog(null,"Please choose one image to query!");
+
+		if (e.getActionCommand().equals("Database")) {
+			JFileChooser directoryChooser = new JFileChooser("c:/");
+			directoryChooser.setFileSelectionMode(1);// 璁惧�������介����╁�版��浠跺す
+			int result = directoryChooser.showOpenDialog(this);// 姝ゅ�ユ�����寮����浠堕����╁�ㄧ����㈢��瑙����璇����
+			if (result == JFileChooser.APPROVE_OPTION) {
+				File f = directoryChooser.getSelectedFile();// f涓洪����╁�扮�����浠�
+				databasePath = f.getAbsolutePath();
+				// System.out.println(f.getAbsolutePath());
 			}
+		}
+
+		if (e.getActionCommand().equals("Search")) {
+			if (picName == "") {
+				javax.swing.JOptionPane.showMessageDialog(null,
+						"Please choose one image to query!");
+			} 
+			else if ((histogram == "")||(distance == ""))
+			{
+				javax.swing.JOptionPane.showMessageDialog(null,
+						"Please choose the query methods!");
+			}	
 			else
 			{
 				try {
-					tool=new Tool();
-					result=tool.Cbir(1,24,picName);
+					tool = new Tool();
+					result = tool.Cbir(1, 24, picName);
 
-					System.out.println("the result is"+result[0]);
-					String namesString=result[0].toString();
-					String []sortFileName=namesString.split(",");
-//					System.out.println("sorted filenames are"+sortFileName[1]);
-					
-					for (int i = 0; i < 24; i++)
-					{
-//						ImageIcon icon = new ImageIcon("pic.png"); 
+					System.out.println("the result is" + result[0]);
+					String namesString = result[0].toString();
+					String[] sortFileName = namesString.split(",");
+					// System.out.println("sorted filenames are"+sortFileName[1]);
 
-						String qString="/image/"+sortFileName[i];
-						ImageIcon icon = new ImageIcon(this.getClass().getResource(qString)); 					
-						//锟斤拷锟饺憋拷锟斤拷锟斤拷  
-						Image temp = icon.getImage().getScaledInstance(160, 160, icon.getImage().SCALE_DEFAULT);  
+					for (int i = 0; i < 24; i++) {
+						// ImageIcon icon = new ImageIcon("pic.png");
+
+						String qString = "/image/" + sortFileName[i];
+						ImageIcon icon = new ImageIcon(this.getClass()
+								.getResource(qString));
+						// �����ゆ�烽��楗烘����烽����ゆ�烽����ゆ��
+						Image temp = icon.getImage().getScaledInstance(160,
+								160, icon.getImage().SCALE_DEFAULT);
 						icon = new ImageIcon(temp);
 						Image[i].setIcon(icon);
-						
-					}					
+
+					}
 				} catch (MWException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
-//				for (int i = 0; i < 24; i++)
-//				{
-////					ImageIcon icon = new ImageIcon("pic.png"); 
-//					String sortFileName="";
-//					String qString="/image/pic.png"+sortFileName;
-//					ImageIcon icon = new ImageIcon(this.getClass().getResource(qString)); 					
-//					//锟斤拷锟饺憋拷锟斤拷锟斤拷  
-//					Image temp = icon.getImage().getScaledInstance(160, 160, icon.getImage().SCALE_DEFAULT);  
-//					icon = new ImageIcon(temp);
-//					Image[i].setIcon(icon);
-//					
-//				}
-				
+
+				// for (int i = 0; i < 24; i++)
+				// {
+				// // ImageIcon icon = new ImageIcon("pic.png");
+				// String sortFileName="";
+				// String qString="/image/pic.png"+sortFileName;
+				// ImageIcon icon = new
+				// ImageIcon(this.getClass().getResource(qString));
+				// //�����ゆ�烽��楗烘����烽����ゆ�烽����ゆ��
+				// Image temp = icon.getImage().getScaledInstance(160, 160,
+				// icon.getImage().SCALE_DEFAULT);
+				// icon = new ImageIcon(temp);
+				// Image[i].setIcon(icon);
+				//
+				// }
+
 				basicPanel.remove(introduction);
 				basicPanel.add(panel2, "Center");
 				basicPanel.add(bottom, "South");
@@ -324,9 +318,8 @@ class Frame extends JFrame implements ActionListener
 				System.out.println(distance);
 			}
 		}
-		
-		if (e.getActionCommand().equals("Previous"))
-		{
+
+		if (e.getActionCommand().equals("Previous")) {
 			previous.setEnabled(false);
 			next.setEnabled(true);
 			basicPanel.remove(panel3);
@@ -334,9 +327,8 @@ class Frame extends JFrame implements ActionListener
 			basicPanel.revalidate();
 			basicPanel.repaint();
 		}
-		
-		if (e.getActionCommand().equals("Next"))
-		{
+
+		if (e.getActionCommand().equals("Next")) {
 			previous.setEnabled(true);
 			next.setEnabled(false);
 			basicPanel.remove(panel2);
@@ -344,6 +336,5 @@ class Frame extends JFrame implements ActionListener
 			basicPanel.revalidate();
 			basicPanel.repaint();
 		}
-    }
+	}
 }
-
